8c2233dbbce2aa3bcf22cae01ed7c120180cd691
Kenneth Porterfield <kjport3@gmail.com>
2021-05-09 14:07:08 -0400

    Added Dracula theme to code highlighting

--- includes/header.php
+++ includes/header.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" type="text/css" href="external/css/grid.css">
     <link rel="stylesheet" type="text/css" href="external/css/ionicons.min.css">
     <link rel="stylesheet" type="text/css" href="external/css/animate.css">
+    <link rel="stylesheet" type="text/css" href="external/css/dracula.css">
     <link rel="stylesheet" type="text/css" href="resources/css/style.css">
     <link rel="stylesheet" type="text/css" href="resources/css/queries.css">
     <link rel="stylesheet" type="text/css" href="admin/font-awesome/css/font-awesome.min.css">
